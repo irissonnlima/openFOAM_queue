e6799a830007f07c75fd25292248bc001d098f4f
Irisson N Lima <irisson220@gmail.com>
2022-06-15 15:33:54 -0300

    Organização da interface de comando

--- CL_corrector.py
+++ CL_corrector.py
@@ -4,44 +4,38 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-params  = sys.argv[1:]
-Path    = params[0] + '/postProcessing/WallCoeffsCyl1/0/forceCoeffs.dat' 
+def justWatch(case_name:str, lim:int, dark_mode = False):
+    Path    = case_name + '/postProcessing/WallCoeffsCyl1/0/forceCoeffs.dat' 
 
-values  = np.loadtxt(Path)
+    #plt.style.use('dark_background')
 
-xa = values[:,0]
-y1 = values[:,3]
-y2 = values[:,2]
+    plt.ion()
+    figure, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex=True)
+    line1, = ax1.plot(xa, y1, label=r'Cl $\alpha=0°$', linewidth=1)
+    line2, = ax2.plot(xa, y2, label=r'Cd $\alpha=0°$', linewidth=1)
 
-plt.ion()
+    ax1.set_title('Cl evolution')
+    ax1.grid(linestyle='-.', linewidth=0.5)
+    ax1.legend()
 
-figure, (ax1, ax2) = plt.subplots(1,2)
-line1, = ax1.plot(xa, y1, label=r'Cl $\alpha=0°$', linewidth=0.5)
-line2, = ax2.plot(xa, y2, label=r'Cd $\alpha=0°$', linewidth=0.5)
+    ax2.set_title('Cd evolution')
+    ax2.grid(linestyle='-.', linewidth=0.5)
+    ax2.legend()
 
-ax1.set_title('Cl evolution')
-ax1.set(xlabel='Time Step', ylabel='Cl')
-ax1.grid(linestyle='-.', linewidth=0.5)
-ax1.legend()
+    while(len(values) < lim):
+        values  = np.loadtxt(Path)
+        time    = values[:, 0]
+        cd      = values[:, 2]
+        cl      = values[:, 3]
+        
+        line1.set_xdata(time)
+        line1.set_ydata(cl)
+        
+        line2.set_xdata(time)
+        line2.set_ydata(cd)
+        
+        figure.canvas.draw()
+        
+        figure.canvas.flush_events()
 
-ax2.set_title('Cd evolution')
-ax2.set(xlabel='Time Step', ylabel='Cd')
-ax2.grid(linestyle='-.', linewidth=0.5)
-ax2.legend()
-
-while(len(values) < tam):
-    xa = values[:, 0]
-    y1 = values[:, 3]
-    y2 = values[:, 2]
-    
-    line1.set_xdata(xa)
-    line1.set_ydata(y1)
-    
-    line2.set_xdata(xa)
-    line2.set_ydata(y2)
-    
-    figure.canvas.draw()
-    
-    figure.canvas.flush_events()
-    #time.sleep(0.01)
 
